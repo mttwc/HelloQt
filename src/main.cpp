19158d071694fb15ad89633668b7508057d1570a
mttwc <github@mttw.xyz>
2015-12-26 19:24:47 +1300

    Demonstrates tooltip when hovering over the window

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,8 @@ int main(int argc, char *argv[])
     QWidget window;
     
     window.resize(250, 150);
-    window.setWindowTitle("Hello world");
+    window.setWindowTitle("Tooltip");
+    window.setToolTip("QWidget");
     window.show();
     
     return app.exec();
